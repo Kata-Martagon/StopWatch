7d36b0d4d689ec36b66558d53e95060f42d18c88
Matt Glover <matthew.glover@peeq-digital.com>
2016-06-15 14:31:59 +0100

    Add describe blocks and tidy up tests

---
+++ spec/environmentCheckSpec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+describe("Test environment works", function() {
+
+  var timerCallback;
+
+  // Install jasmine.clock().install
+  beforeEach(function() {
+    timerCallback = jasmine.createSpy("timerCallback");
+    jasmine.clock().install();
+  });
+
+  // Uninstall clock
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it("causes a timeout to be called synchronously", function() {
+    setTimeout(function() {
+      timerCallback();
+    }, 100);
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.clock().tick(101);
+
+    expect(timerCallback).toHaveBeenCalled();
+  });
+
+  it("should create a successful test environment", function() {
+    expect(true).toBe(true);
+  });
+});
