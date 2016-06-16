c17ebbbec5e71157ff822f9ca14a0c6e2de62ac6
Matt Glover <matthew.glover@peeq-digital.com>
2016-06-16 15:55:36 +0100

    FEAT: enable lap time display

---
+++ src/Animator.js
@@ -0,0 +1,27 @@
+var Animator = {
+  _ticking: false,
+
+  _callbacks: [],
+
+  addCallback: function (cb) {
+    Animator._callbacks.push(cb);
+  },
+
+  start: function () {
+    if (Animator._ticking) return;
+    Animator._ticking = true;
+    window.requestAnimationFrame(Animator._runAnimation);
+  },
+
+  stop: function () {
+    Animator._ticking = false;
+  },
+
+  _runAnimation: function () {
+    // Run callbacks
+    Animator._callbacks.forEach(function (cb) { cb(); });
+
+    // If ticking then set to run on next ticking
+    if (Animator._ticking) window.requestAnimationFrame(Animator._runAnimation);
+  }
+};
