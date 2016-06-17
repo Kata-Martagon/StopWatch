52f8c77d79d5687d07cbd820714bd69f8d0b8de2
Matt Glover <matthew.glover@peeq-digital.com>
2016-06-17 10:36:12 +0100

    Simplify laps

--- src/app.js
+++ src/app.js
@@ -1,93 +1,139 @@
-// var lastList = [];
+(function () {
+  'use strict';
 
-function updateTime() {
-  document.getElementById('Timer').textContent = formatTime(stopWatch.getTimeElapsed());
-  document.getElementById('LapTimer').textContent = formatTime(stopWatch.getTimeElapsedFromLastLap());
-}
+  var MainTimerDiv = document.getElementById('Timer');
+  var LapTimerDiv = document.getElementById('LapTimer');
+  var stopWatch;
 
-var stopWatch = new StopWatch();
+  reset();
 
-stopWatch.subscribeToLapUpdate(updateLapList);
+  window.reset = reset;
 
-updateTime();
+  function reset () {
+    clearLapList();
 
-Animator.addCallback(updateTime);
+    Animator.clearCallbacks();
+    Animator.addCallback(updateTimers);
 
+    stopWatch = new StopWatch();
+    stopWatch.subscribeToLapUpdate(updateLapList);
 
-function formatNumber (num) {
-  return num < 10 ? '0' + num : num;
-}
-
-function start() {
-  stopWatch.start();
-  hideButton('StartButton');
-  hideButton('ResetButton');
-  showButton('StopButton');
-  showButton('LapButton');
-  Animator.start();
-}
-
-function stop() {
-  stopWatch.stop();
-  hideButton('StopButton');
-  hideButton('LapButton');
-  showButton('StartButton');
-  showButton('ResetButton');
-  Animator.stop();
-}
-
-function reset () {
-  stopWatch = new StopWatch();
-  Animator.stop();
-  updateTime();
-  clearLapList();
-  stopWatch.subscribeToLapUpdate(updateLapList);
-}
-
-function lap() {
-  stopWatch.recordLap();
-}
-
-function hideButton (elementId) {
-  document.getElementById(elementId).style.display = 'none';
-}
-
-function showButton (elementId) {
-  document.getElementById(elementId).style.display = 'block';
-}
-
-function formatTime (timeMS) {
-  // if (!Array.isArray(timeArr) || timeArr.length < 3) return;
-  var timeArr = TimeConverter.convertTimeToArray(timeMS);
-  return formatNumber(timeArr[0]) + '.' + formatNumber(timeArr[1]) + ':' + formatNumber(timeArr[2]);
-}
-
-function updateLapList (list) {
-  var listNode = document.getElementById('LapTimes');
-
-  clearLapList();
-  buildLapList(list);
-}
-
-function clearLapList () {
-  var listNode = document.getElementById('LapTimes');
-
-  while (listNode.hasChildNodes()) {
-    listNode.removeChild(listNode.firstChild);
+    updateTimers();
   }
-}
 
-function buildLapList (list) {
-  list.reverse().forEach(creatTableRow);
-}
+  function updateTimers() {
+    MainTimerDiv.textContent = formatTime(stopWatch.getTimeElapsed());
+    LapTimerDiv.textContent = formatTime(stopWatch.getTimeElapsedFromLastLap());
+  }
 
-function creatTableRow (time, idx, arr) {
-  var trNode = document.createElement('tr');
-  var tdDescription = document.createElement('td');
-  tdDescription.textContent = 'Lap ' + (arr.length - idx);
-  var tdTime = document.createElement('td');
-  tdTime.textContent = formatTime(time);
-  trNode.appendChild(tdDescription);
-  trNode.appendChild(tdTime);
-  document.getElementById('LapTimes').appendChild(trNode);
-}
+  function formatNumber (num) {
+    return num < 10 ? '0' + num : num;
+  }
+
+  window.start = function start() {
+    stopWatch.start();
+    hideButton('StartButton');
+    hideButton('ResetButton');
+    showButton('StopButton');
+    showButton('LapButton');
+    Animator.start();
+  };
+
+  window.stop = function stop() {
+    stopWatch.stop();
+    hideButton('StopButton');
+    hideButton('LapButton');
+    showButton('StartButton');
+    showButton('ResetButton');
+    Animator.stop();
+  };
+
+  window.lap = function lap() {
+    stopWatch.recordLap();
+  };
+
+  function hideButton (elementId) {
+    document.getElementById(elementId).style.display = 'none';
+  }
+
+  function showButton (elementId) {
+    document.getElementById(elementId).style.display = 'block';
+  }
+
+  function formatTime (timeMS) {
+    // if (!Array.isArray(timeArr) || timeArr.length < 3) return;
+    var timeArr = TimeConverter.convertTimeToArray(timeMS);
+    return formatNumber(timeArr[0]) + '.' + formatNumber(timeArr[1]) + ':' + formatNumber(timeArr[2]);
+  }
+
+  function updateLapList (list) {
+    clearLapList();
+    buildLapList(list);
+  }
+
+  function clearLapList () {
+    var listNode = document.getElementById('LapTimes');
+
+    while (listNode.hasChildNodes()) {
+      listNode.removeChild(listNode.firstChild);
+    }
+  }
+
+  function buildLapList (list) {
+    list.reverse().forEach(creatTableRow);
+  }
+
+  function creatTableRow (time, idx, arr) {
+    var trNode = document.createElement('tr');
+    var tdDescription = document.createElement('td');
+    tdDescription.textContent = 'Lap ' + (arr.length - idx);
+    var tdTime = document.createElement('td');
+    tdTime.textContent = formatTime(time);
+    trNode.appendChild(tdDescription);
+    trNode.appendChild(tdTime);
+    document.getElementById('LapTimes').appendChild(trNode);
+  }
+})();
+// function updateTime() {
+//   document.getElementById('Timer').textContent = formatTime(stopWatch.getTimeElapsed());
+//   document.getElementById('LapTimer').textContent = formatTime(stopWatch.getTimeElapsedFromLastLap());
+// }
+
+// var stopWatch = new StopWatch();
+
+// stopWatch.subscribeToLapUpdate(updateLapList);
+//
+// updateTime();
+//
+// Animator.addCallback(updateTime);
+
+
+// function formatNumber (num) {
+//   return num < 10 ? '0' + num : num;
+// }
+
+// function start() {
+//   stopWatch.start();
+//   hideButton('StartButton');
+//   hideButton('ResetButton');
+//   showButton('StopButton');
+//   showButton('LapButton');
+//   Animator.start();
+// }
+
+// function stop() {
+//   stopWatch.stop();
+//   hideButton('StopButton');
+//   hideButton('LapButton');
+//   showButton('StartButton');
+//   showButton('ResetButton');
+//   Animator.stop();
+// }
+
+// function reset () {
+//   stopWatch = new StopWatch();
+//   Animator.stop();
+//   updateTime();
+//   clearLapList();
+//   stopWatch.subscribeToLapUpdate(updateLapList);
+// }
